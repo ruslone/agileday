f7583583d069ffc0c157dcfcba7d51a1bbb5fd83
Petr Kozachenko <pkozachenko@alfabank.ru>
2026-01-16 07:53:54 +0300

    bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,11 @@
-import { Telegraf } from 'telegraf';
-import session from 'telegraf/session';
+import { Telegraf, session } from 'telegraf'; // session теперь импортируется из telegraf
 
 import registerCommands from './commands/index.js';
 import registerActions from './actions/index.js';
 
 export const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.use(session());
+bot.use(session()); // теперь это работает
 
 registerCommands(bot);
 registerActions(bot);
