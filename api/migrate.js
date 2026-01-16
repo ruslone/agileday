857fc111279a566868af9d7e452887a19a95ff79
Petr Kozachenko <pkozachenko@alfabank.ru>
2026-01-16 08:05:38 +0300

    migrate.js

---
+++ api/migrate.js
@@ -0,0 +1,17 @@
+import { sql } from '../src/db.js';
+import fs from 'fs';
+
+const sqlText = fs.readFileSync('../src/migration.sql', 'utf-8');
+
+async function migrate() {
+  try {
+    await sql.unsafe(sqlText);
+    console.log('Миграция выполнена');
+    process.exit(0);
+  } catch (err) {
+    console.error('Ошибка миграции', err);
+    process.exit(1);
+  }
+}
+
+migrate();
